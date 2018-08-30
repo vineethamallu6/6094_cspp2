e7c5fc83c89fbdf88f971ab09afa633fca6f6af4
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-29 18:42:48 -0700

    i

---
+++ cspp2-practice/m3/ConvertToWhileLoop.java
@@ -0,0 +1,19 @@
+public class ConvertToWhileLoop
+{
+	public static void main(String[] args)
+	{
+		int i=0;
+		while(i<10)
+		{
+			i=i+2;
+		    System.out.println(i);
+			
+			if (i==10)
+		    {
+				System.out.println("GoodBye!");
+		    }
+
+		}
+	  
+	}
+}
