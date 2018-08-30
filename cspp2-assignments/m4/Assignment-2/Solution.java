0430c059f0b0c274e5c623934ec9c8e80fc9b3ad
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-30 14:35:05 -0700

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 public final class  Solution{
 	public static void main(final String[] args) {
-		int m,n,i,j;
+		int m,n,i,j,p,q,k,l;
 		Scanner s=new Scanner(System.in);
 		m=s.nextInt();
 		n=s.nextInt();
+		
+
 		int arr1[][]=new int[m][n];
-		int arr2[][]= new int[m][n];
-		int sum[][]=new int[m][n];
+		
 		for (i=0;i<m;i++)
 		{
 			for (j=0;j<n;j++)
@@ -15,27 +16,32 @@ public final class  Solution{
 				arr1[i][j]=s.nextInt();
 			}
 		}
-		for (i=0;i<m;i++)
+		p=s.nextInt();
+		q=s.nextInt();
+		int arr2[][]= new int[p][q];
+		int sum[][]=new int[m][q];
+
+		for (k=0;k<p;k++)
 		{
-			for (j=0;j<n;j++)
+			for (l=0;l<q;l++)
 			{
-				arr2[i][j]=s.nextInt();
+				arr2[k][l]=s.nextInt();
 			}
 		}
 		for (i=0;i<m;i++)
 		{
-			for (j=0;j<n;j++)
+			for (l=0;l<q;l++)
 			{
-				sum[i][j]=arr1[i][j]+arr2[i][j];
+				sum[i][l]=arr1[i][l]+arr2[i][l];
 			}
 		}
 		 for (i=0;i<m;i++)
 		{
-			for (j=0;j<n;j++)
+			for (l=0;l<q;l++)
 			{
-				System.out.print(sum[i][j]+"\t");
+				System.out.print(sum[i][l]);
 			}
-			System.out.println();
+			System.out.println(" ");
 		}
 	}
 }
