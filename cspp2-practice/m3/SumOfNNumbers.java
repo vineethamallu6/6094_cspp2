f87ed3fbcee407fa24fe49d0ce77b8e2e48b8198
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-29 18:48:17 -0700

    v

---
+++ cspp2-practice/m3/SumOfNNumbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class SumOfNNumbers
+{
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int sum=0;
+		for(int i=1;i<=n;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.println("sum= "+sum);
+	}
+}
