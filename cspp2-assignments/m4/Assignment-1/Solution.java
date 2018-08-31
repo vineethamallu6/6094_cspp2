add05bae5ed1f699b4da25eb26d2cf55034216e3
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-30 18:39:15 -0700

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-1/Solution.java
+++ cspp2-assignments/m4/Assignment-1/Solution.java
@@ -5,27 +5,27 @@ import java.util.Scanner;
 */
 public class Solution {
 /**Fill.
-	*/
+    */
     private Solution() {
-		/**constructor.
-		*/
-	}
-	/**main function.
-	*@param args String
-	*/
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int arr[] = new int[n];
-		for (int i = 0;i < n;i++) {
-			arr[i] = sc.nextInt();
-		}
-		int max = arr[0];
-		for (int i = 0; i < n; i++) {
-			if (max < arr[i]) {
-				max = arr[i];
-			}
-		}
-		System.out.println(max);
-	}
+        /**constructor.
+        */
+    }
+    /**main function.
+    *@param args String
+    */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0;i < n;i++) {
+            arr[i] = sc.nextInt();
+        }
+        int max = arr[0];
+        for (int i = 0; i < n; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+    }
 }
