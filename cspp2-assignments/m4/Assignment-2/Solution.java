4b47cdcbce636633c933a1cc6dac436c870080ac
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-30 18:47:16 -0700

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -8,10 +8,8 @@ public final class  Solution{
 		
 			int[][] arr1 = new int[m][n];
 		
-		for (i = 0; i < m; i++)
-		{
-			for (j = 0; j < n; j++)
-			{
+		for (i = 0; i < m; i++) {
+			for (j = 0; j < n; j++) {
 				arr1[i][j] = s.nextInt();
 			}
 		}
@@ -20,34 +18,25 @@ public final class  Solution{
 		int arr2[][] = new int[p][q];
 		int sum[][] = new int[m][q];
 
-		for (k = 0; k < p; k++)
-		{
-			for (l = 0; l < q; l++)
-			{
+		for (k = 0; k < p; k++) {
+			for (l = 0; l < q; l++) {
 				arr2[k][l]=s.nextInt();
 			}
 		}
-		if (m == p && n == q){
-
-
-		for (i = 0; i < m; i++)
-		{
-			for (l = 0; l < q; l++)
-			{
+		if (m == p && n == q) {
+			for (i = 0; i < m; i++) {
+			for (l = 0; l < q; l++) {
 				sum[i][l] = arr1[i][l] + arr2[i][l];
 			}
 		}
-		 for (i = 0; i < m; i++)
-		{
-			for (l = 0; l < q - 1; l++)
-			{
+		 for (i = 0; i < m; i++) {
+			for (l = 0; l < q - 1; l++) {
 				System.out.print(sum[i][l] + " ");
 			}
 			System.out.println(sum[i][q - 1]);
 		}
 	}
-	else
-	{
+	else {
 		System.out.println("not possible");
 	}
 }
