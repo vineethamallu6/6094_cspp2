4ba406f391f205b5b3d7bbbf358c591ad4a2cbb0
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-30 18:44:57 -0700

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -1,49 +1,49 @@
 import java.util.Scanner;
 public final class  Solution{
 	public static void main(final String[] args) {
-		int m,n,i,j,p,q,k,l;
-		Scanner s=new Scanner(System.in);
-		m=s.nextInt();
-		n=s.nextInt();
+		int m, n, i, j, p, q, k, l;
+		Scanner s = new Scanner(System.in);
+		m = s.nextInt();
+		n = s.nextInt();
 		
-			int arr1[][]=new int[m][n];
+			int[][] arr1 = new int[m][n];
 		
-		for (i=0;i<m;i++)
+		for (i = 0; i < m; i++)
 		{
-			for (j=0;j<n;j++)
+			for (j = 0; j < n; j++)
 			{
-				arr1[i][j]=s.nextInt();
+				arr1[i][j] = s.nextInt();
 			}
 		}
-		p=s.nextInt();
-		q=s.nextInt();
-		int arr2[][]= new int[p][q];
-		int sum[][]=new int[m][q];
+		p = s.nextInt();
+		q = s.nextInt();
+		int arr2[][] = new int[p][q];
+		int sum[][] = new int[m][q];
 
-		for (k=0;k<p;k++)
+		for (k = 0; k < p; k++)
 		{
-			for (l=0;l<q;l++)
+			for (l = 0; l < q; l++)
 			{
 				arr2[k][l]=s.nextInt();
 			}
 		}
-		if (m==p&&n==q){
+		if (m == p && n == q){
 
 
-		for (i=0;i<m;i++)
+		for (i = 0; i < m; i++)
 		{
-			for (l=0;l<q;l++)
+			for (l = 0; l < q; l++)
 			{
-				sum[i][l]=arr1[i][l]+arr2[i][l];
+				sum[i][l] = arr1[i][l] + arr2[i][l];
 			}
 		}
-		 for (i=0;i<m;i++)
+		 for (i = 0; i < m; i++)
 		{
-			for (l=0;l<q-1;l++)
+			for (l = 0; l < q - 1; l++)
 			{
-				System.out.print(sum[i][l]+" ");
+				System.out.print(sum[i][l] + " ");
 			}
-			System.out.println(sum[i][q-1]);
+			System.out.println(sum[i][q - 1]);
 		}
 	}
 	else
