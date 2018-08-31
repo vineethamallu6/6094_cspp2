c624c01aa8bcd39a7029d9bfa95594daa536c93e
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-31 15:03:05 -0700

    "check_style_score -> 1 of 1 passed. style score: 1/1 "

--- cspp2-assignments/m4/Assignment-3/Solution.java
+++ cspp2-assignments/m4/Assignment-3/Solution.java
@@ -5,42 +5,45 @@ import java.util.Scanner;
 */
 public final class Solution {
 /**
-	Do not modify this main function.
-	*/
-	public static final int N_U_M = 10;
-	/**
-	Number.
-	*/
-	private Solution() {
-	/**constructtor.
-	*/
-	}
-	/**main function.
-	*@param args String
-	*/
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		for (int i = 0; i < n; i++) {
-			String s = sc.next();
-			String res = binaryToDecimal(s);
-			System.out.println(res);
-		}
-	}
-	/**function.
-	*@param str String.
+    Do not modify this main function.
     */
-	public static String binaryToDecimal(final String str) {
-		 int decimal = 0, i = 0;
-		 long n = Long.parseLong(str);
-		 while (n != 0) {
-		 	decimal += ((n % N_U_M) * Math.pow(2, i));
-		 	n = n / N_U_M;
-		 	i++;
-		 }
-		 String s1 = decimal + "";
-		 return s1;
+    public static final int N_U_M = 10;
+    /**
+    Number.
+    */
+    private Solution() {
+    /**constructtor.
+    */
+    }
+    /**main function.
+    *@param args String
+    */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            String res = binaryToDecimal(s);
+            System.out.println(res);
+        }
+    }
+    /**
+    function.
+    *@return String
+    /**
+    *@param str String.
+    */
+    public static String binaryToDecimal(final String str) {
+         int decimal = 0, i = 0;
+         long n = Long.parseLong(str);
+         while (n != 0) {
+            decimal += ((n % N_U_M) * Math.pow(2, i));
+            n = n / N_U_M;
+            i++;
+         }
+         String s1 = decimal + "";
+         return s1;
 
-	}
+    }
 
 }
