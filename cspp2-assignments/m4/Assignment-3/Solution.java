79628db84cf9e2f7bd684708dcb2c326d6fecd0f
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-08-31 13:51:14 -0700

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-3/Solution.java
+++ cspp2-assignments/m4/Assignment-3/Solution.java
@@ -6,23 +6,23 @@ public class Solution
 	Do not modify this main function.
 	*/
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
-		for(int i=0;i<n;i++) {
-			String s=sc.next();
-			String res=binaryToDecimal(s);//Write binaryToDecimal function
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 0; i < n; i ++) {
+			String s = sc.next();
+			String res = binaryToDecimal(s);//Write binaryToDecimal function
 			System.out.println(res);
 		}
 	}
 	public static String binaryToDecimal(String str) {
-		 int decimal=0,i=0;
-		 long n=Long.parseLong(str);
-		 while(n!=0) {
-		 	decimal+=((n%10)*Math.pow(2,i));
-		 	n=n/10;
-		 	i++;
+		 int decimal = 0,i = 0;
+		 long n = Long.parseLong(str);
+		 while(n != 0) {
+		 	decimal += ((n % 10) * Math.pow(2, i));
+		 	n = n / 10;
+		 	i ++;
 		 }
-		 String s1=decimal+"";
+		 String s1 = decimal + "";
 		 return s1;
 
 	}
