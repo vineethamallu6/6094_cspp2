62ce7f9d616ef5e24140e99ba818e61f8056b8d7
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-09-01 11:35:15 -0700

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

---
+++ cspp2-assignments/m6/Assignment-2/Solution.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+/**
+ * Write a java program to round the
+ * elements of a matrix to the nearest 100.
+ *
+ * @author : 
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //not used
+    }
+    /**
+     * Function to round the
+     * elements of a matrix to the nearest 100.
+     *
+     * @param      a     Matrix of which the elements to be rounded
+     * @param      rows     Number of rows
+     * @param      columns     Number of columns
+     *
+     * @return     Matrix of the rounded elements
+     */
+    static int[][] roundHundred(final int[][] a, final int rows, final int columns) {
+        int i,j;
+        for (i=0; i<rows; i++) 
+        {
+            for (j=0; j<columns; j++){
+                int value=a[i][j];
+                System.out.println(value);
+                if(value>50) {
+                    a[i][j]=100;
+                }
+                else{
+                    a[i][j]=0;
+                }
+            
+        }
+        
+    }
+
+      return a;
+	// write ypur code here
+	
+    }
+
+    /**
+     * Main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        int[][] a = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = scan.nextInt();
+            }
+        }
+        int[][] b = roundHundred(a, m, n);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                System.out.print(b[i][j] + " ");
+            }
+            System.out.println(b[i][n - 1]);
+        }
+    }
+}
