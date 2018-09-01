bfd3a0310fcfb6a4fd92badd551b63509eb77349
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-09-01 12:24:17 -0700

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- cspp2-assignments/m6/Assignment-1/Solution.java
+++ cspp2-assignments/m6/Assignment-1/Solution.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
  * Write a java program to find the odd composite numbers
  * between 2 and n where n is given as an input.
  *
- * @author : 
+ * @author :
  */
 public final class Solution {
     /**
@@ -18,19 +18,19 @@ public final class Solution {
      * @param      n     n value
      */
     static void oddComposites(final int n) {
-    	int count = 0, i, j;
-    	for (i = 1; i < n; i++) {
-    	count = 0;
-    		if (i % 2 == 1) {
-    		count = 0;
-    		for (j = 1; j <= i; j++) {
-    			if (i % j == 0) {
-    				count = count + 1;
-    			}
-    		}
-    		if (count > 2) {
-    		System.out.println(i);
-    	     }
+        int count = 0, i, j;
+        for (i = 1; i < n; i++) {
+        count = 0;
+            if (i % 2 == 1) {
+            count = 0;
+            for (j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    count = count + 1;
+                }
+            }
+            if (count > 2) {
+            System.out.println(i);
+             }
         }
     }
 }
