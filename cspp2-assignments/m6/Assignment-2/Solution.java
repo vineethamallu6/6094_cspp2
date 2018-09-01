2fc8d5eb1229cd0cd35a0cfea23ed2f21382d4c7
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-09-01 11:56:55 -0700

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m6/Assignment-2/Solution.java
+++ cspp2-assignments/m6/Assignment-2/Solution.java
@@ -28,28 +28,49 @@ final class Solution {
         {
             for (j=0; j<columns; j++){
                 int value=a[i][j];
-                if(value>50) {
+                if(value<100 && value>0) {
+                    if(value>=50)
                     a[i][j]=100;
+                    else
+                    a[i][j]=0;
                 }
-                else if(value>100 && value<200)
+                if(value>100 && value<200)
                 {
-                    a[i][j]=200;
+                    if(value<150)
+                      a[i][j]=100;
+                    else
+                      a[i][j]=200;  
                 }
-                else if(value>300 && value<400)
+                else if(value>200 && value<300)
+                { 
+                    if(value<250)
+                      a[i][j]=200;
+                    else
+                      a[i][j]=300; 
+              }
+                else if(value>300 && value<=400)
                 {
-                    a[i][j]=300;
+                    if(value<350)
+                      a[i][j]=300;
+                    else
+                      a[i][j]=400; 
                 }
-                else if(value>400 && value<=500)
+                else if(value>400 && value<500)
                 {
-                    a[i][j]=400;
+                    if(value<450)
+                      a[i][j]=400;
+                    else
+                      a[i][j]=500; 
                 }
                 else if(value>500 && value<600)
                 {
-                    a[i][j]=500;
+                    if(value<550)
+                      a[i][j]=500;
+                    else
+                      a[i][j]=600; 
                 }
-                else{
-                    a[i][j]=0;
-                }
+                
+                
             
         }
         
