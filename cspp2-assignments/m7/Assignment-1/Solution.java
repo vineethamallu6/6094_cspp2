baa39fd4cc096f934a83b9986cc486caf37445a8
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-09-03 20:36:12 -0700

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m7/Assignment-1/Solution.java
+++ cspp2-assignments/m7/Assignment-1/Solution.java
@@ -16,9 +16,9 @@ private int length;
  *
  *@param      input is input.
  */
-public InputValidator(String input) {
- 	data = input;
- 	length = input.length();
+public InputValidator(final String input) {
+    data = input;
+    length = input.length();
  }
  /**
  * function to return true or false based on given input.
@@ -26,30 +26,30 @@ public InputValidator(String input) {
  *@return     return true or false.
  */
  boolean validateData() {
- 	final int var = 6;
- 	if (length >= var) {
- 		return true;
- 	}
- 	return false;
- 	}
+    final int var = 6;
+    if (length >= var) {
+        return true;
+    }
+    return false;
+    }
  }
  /**
  * class Solution that contains main method.
  */
 final class Solution {
-	/**constructor for solution.
-	*/
-	private Solution() {
+    /**constructor for solution.
+    */
+    private Solution() {
 }
 /**
 * main function.
 *
 *@param    args  the arguments
 */
-	public static void main(String args[]) {
-    	Scanner s = new Scanner(System.in);
-    	String input = s.next();
-    	InputValidator i = new InputValidator(input);
-    	System.out.println(i.validateData());
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);
+        System.out.println(i.validateData());
     }
 }
