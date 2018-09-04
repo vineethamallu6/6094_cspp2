2316d31ba355d81ee5ac1c2745ac6438e00adccc
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-09-03 20:21:56 -0700

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ cspp2-assignments/m7/Assignment-1/Solution.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class InputValidator
+{
+ int length;
+ String data;
+public InputValidator(String input)
+ {
+ 	data=input;
+ 	length = input.length();
+ }
+ boolean validateData() {
+ 	final int var = 6;
+ 	if(length >= var) {
+ 		return true;
+ 	}
+ 	return false;
+ 	}
+ }
+
+public class Solution
+{
+	public static void main(String args[])
+    {
+    	Scanner s = new Scanner(System.in);
+    	String input = s.next();
+    	InputValidator i = new InputValidator(input);    	
+    	System.out.println(i.validateData());
+
+    }
+
+}
