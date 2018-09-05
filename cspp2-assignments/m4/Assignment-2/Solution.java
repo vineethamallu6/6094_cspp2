450a3ee71eaade85daac1e9571023fe7bc632f54
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-09-05 14:10:03 -0700

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -18,33 +18,33 @@ public final class  Solution {
         Scanner s = new Scanner(System.in);
         m = s.nextInt();
         n = s.nextInt();
-        int[][] arr1 = new int[m][n];
+        int[][] matrixOne = new int[m][n];
         for (i = 0; i < m; i++) {
             for (j = 0; j < n; j++) {
-                arr1[i][j] = s.nextInt();
+                matrixOne[i][j] = s.nextInt();
             }
         }
         p = s.nextInt();
         q = s.nextInt();
-        int[][] arr2 = new int[p][q];
-        int[][] sum = new int[m][q];
+        int[][] matrixTwo = new int[p][q];
+        int[][] resultMatrix = new int[m][q];
 
         for (k = 0; k < p; k++) {
             for (l = 0; l < q; l++) {
-                arr2[k][l] = s.nextInt();
+                matrixTwo[k][l] = s.nextInt();
             }
         }
         if (m == p && n == q) {
             for (i = 0; i < m; i++) {
             for (l = 0; l < q; l++) {
-                sum[i][l] = arr1[i][l] + arr2[i][l];
+                resultMatrix[i][l] = matrixOne[i][l] + matrixTwo[i][l];
             }
         }
          for (i = 0; i < m; i++) {
             for (l = 0; l < q - 1; l++) {
-                System.out.print(sum[i][l] + " ");
+                System.out.print(resultMatrix[i][l] + " ");
             }
-            System.out.println(sum[i][q - 1]);
+            System.out.println(resultMatrix[i][q - 1]);
         }
     } else {
         System.out.println("not possible");
