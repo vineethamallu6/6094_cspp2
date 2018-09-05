b81cec3917c2073a33758b1811eb04276049b86e
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-09-05 11:08:21 -0700

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m9/Assignment-1/Fibonacci.java
+++ cspp2-assignments/m9/Assignment-1/Fibonacci.java
@@ -16,8 +16,7 @@ import java.util.Scanner;
  * by invoking the add method provided in the List ADT.
  *
  */
-final public class Fibonacci
-{
+final public class Fibonacci {
     /*
      * fib is a static method takes a argument n
      * n is the count of the fibonacci numbers to be generated.
