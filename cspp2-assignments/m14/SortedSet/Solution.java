d1e76bad2dc93778d09e92422143f891c6a09217
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-09-10 20:45:20 -0700

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

--- cspp2-assignments/m14/SortedSet/Solution.java
+++ cspp2-assignments/m14/SortedSet/Solution.java
@@ -202,30 +202,50 @@ public static void main(final String[] args) {
             String[] tokens = line.split(" ");
             // based on the list operation invoke the corresponding method
             switch (tokens[0]) {
+            case "size":
+                System.out.println(s.size());
+                break;
+            case "contains":
+                System.out.println(s.
+                    contains(Integer.parseInt(tokens[1])));
+                break;
             case "print":
                 System.out.println(s);
                 break;
-            case "add":
-                int[] intArray = intArray(tokens[1]);
-                s.add(intArray[0]);
-                break;
             case "addAll":
-                intArray = intArray(tokens[1]);
-                s.addAll(intArray);
+                int[] intArray = intArray(tokens[1]);
+                if (intArray.length == 1) {
+                    s.add(intArray[0]);
+                } else {
+                    s.add(intArray);
+                }
                 break;
             case "subSet":
-                intArray = intArray(tokens[1]);
-                int[] t = s.subSet(intArray[0],intArray[1]);
-                if (t != null) {
-                System.out.println(Arrays.toString(t).replace("[","{").replace("]","}"));
+                String[] arrstring = tokens[1].split(",");
+                if (Integer.parseInt(arrstring[0])
+                      > Integer.parseInt(arrstring[1])) {
+                    System.out.println("Invalid Arguments to Subset Exception");
+                } else {
+                    int[] subarray = s.subSet(Integer.parseInt(arrstring[0]),
+                            Integer.parseInt(arrstring[1]));
+                    Set subset = new Set();
+                    subset.add(subarray);
+                    if (subset != null) {
+                        System.out.println(subset);
+                    }
                 }
                 break;
             case "headSet":
-                intArray = intArray(tokens[1]);
-                System.out.println(Arrays.toString(s.headSet(intArray[0])).replace("[","{").replace("]","}"));
+                int[] headarray = s.headSet(Integer.parseInt(tokens[1]));
+                Set headset = new Set();
+                headset.add(headarray);
+                if (headset != null) {
+                    System.out.println(headset);
+                }
                 break;
             case "last":
                 System.out.println(s.last());
+                break;
             default:
                 break;
             }
