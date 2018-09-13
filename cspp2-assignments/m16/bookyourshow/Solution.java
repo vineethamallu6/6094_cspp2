41a2854cd2851cc7d87b875a87d2c6f75b21f711
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-09-12 19:18:13 -0700

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m16/bookyourshow/Solution.java
+++ cspp2-assignments/m16/bookyourshow/Solution.java
@@ -171,7 +171,7 @@ class BookYourShow {
         for (int i = 0; i < showDetails.size(); i++) {
 
             System.out.println(showDetails.get(i).toString() + "," +
-                Arrays.toString(showDetails.get(i).getSeatNumbers()).replace(" ",""));
+            Arrays.toString(showDetails.get(i).getSeatNumbers()).replace(" ",""));
         }
     }
 }
