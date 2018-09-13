05d9acf1922f89d34da9ccaa0443427ae90690d7
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-09-12 19:00:00 -0700

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

---
+++ cspp2-assignments/m16/bookyourshow/Solution.java
@@ -0,0 +1,207 @@
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+import java.util.Arrays;
+/**
+ * Class for show.
+ */
+class Show {
+	private String nameOfTheMovie;
+	private String dateAndTime;
+	private String[] seatNumbers;
+	/**
+	 * Constructor for the class show.
+	 *
+	 * @param      m  moviename.
+	 * @param      dt date and time.
+	 * @param      s  seats string.
+	 */
+	Show(String m, String dt, String[] s) {
+		this.nameOfTheMovie = m;
+		this.dateAndTime = dt;
+		this.seatNumbers = s;
+	}
+	/**
+	 * Gets the movie name.
+	 *
+	 * @return returns the movie name.
+	 */
+	public String getMovieName() {
+		return this.nameOfTheMovie;
+	}
+	/**
+	 * Gets the date and time.
+	 *
+	 * @return returns date and time.
+	 */
+	public String getDateAndTime() {
+		return this.dateAndTime;
+	}
+	/**
+	 * Gets the seat numbers.
+	 *
+	 * @return returns the seat numbers.
+	 */
+	public String[] getSeatNumbers() {
+		return this.seatNumbers;
+	}
+	/**
+	 * Sets the seat.
+	 *
+	 * @param index the index variable.
+	 * @param set the set variable to set the name of seat.
+	 */
+	public void setSeat(int index, String set) {
+		seatNumbers[index] = set;
+	}
+	/**
+	 * Returns a string representation of the object.
+	 *
+	 * @return returns string representation of the object.
+	 */
+	public String toString() {
+		return this.nameOfTheMovie + "," + this.dateAndTime;
+	}
+}
+
+class Patron {
+	/**
+	 * username variable.
+	 */
+	private String patronName;
+	/**
+	 * mobile number variable.
+	 */
+	private String mobileNumber;
+	/**
+	 * constructor for patron class.
+	 *
+	 * @param n name of the user.
+	 * @param mbnum mobilenumber of the user.
+	 */
+	Patron(String n, String mbnum) {
+		this.patronName = n;
+		this.mobileNumber = mbnum;
+	}
+	public String getName() {
+		return this.patronName;
+	}
+	public String getMobileNumber() {
+		return this.mobileNumber;
+	}
+}
+
+
+class BookYourShow {
+	List<Show> showDetails;
+	List<String> bookedTickets;
+	public BookYourShow() {
+		//constructor
+		showDetails = new List<Show>();
+		bookedTickets = new List<String>();
+	}
+	public void addAShow(Show obj) {
+		showDetails.add(obj);
+	}
+	public Show getAShow(String movieName, String dateAndTime) {
+		for (int i = 0; i < showDetails.size(); i++) {
+			if (showDetails.get(i).getMovieName().equals(movieName)&&
+				showDetails.get(i).getDateAndTime().equals(dateAndTime)) {
+				return showDetails.get(i);
+			}
+		}
+		return null;
+	}
+	public void bookAShow(String movieName, String dateAndTime, Patron obj, String[] seats) {
+		Show movieData = getAShow(movieName, dateAndTime);
+		if (movieData != null) {
+			for (int i = 0; i < movieData.getSeatNumbers().length; i++) {
+				for (int j = 0; j < seats.length; j++) {
+					if (seats[j].equals(movieData.getSeatNumbers()[i])) {
+						movieData.setSeat(i, "N/A");
+						bookedTickets.add(obj.getMobileNumber() + " " + movieName + " " + dateAndTime);
+					}
+				}
+			}
+		} else {
+			System.out.println("No show");
+		}
+	}
+	public void printTickets(String movieName, String dateAndTime, String mobileNumber) {
+		//print the tickets
+		for (int i = 0; i < bookedTickets.size(); i++) {
+			if ((mobileNumber + " " + movieName + " " + dateAndTime).equals(bookedTickets.get(i))) {
+				System.out.println(mobileNumber + " " + movieName + " " + dateAndTime);
+				return;
+			}
+		}
+		System.out.println("Invalid");
+		return;
+	}
+	public void showAll() {
+		//print all the available shows
+		for (int i = 0; i < showDetails.size(); i++) {
+
+			System.out.println(showDetails.get(i).toString() + "," +
+				Arrays.toString(showDetails.get(i).getSeatNumbers()).replace(" ",""));
+		}
+	}
+}
+
+public class Solution {
+	/**
+	 * main method for class solution.
+	 *
+	 * @param args  The arguments.
+	 */
+   public  static void main(final String[] args) {
+        BookYourShow bys = new BookYourShow();
+        Scanner scan = new Scanner(System.in);
+        int testCases = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < testCases; i++) {
+            String[] tokens = scan.nextLine().
+                replace("[", "").replace("]", "").split(",");
+            String[] check = tokens[0].split(" ");
+            switch (check[0]) {
+                case "add":
+                    int k = 2;
+                    String[] seats = new String[tokens.length - 2];
+                    for (int j = 0; j < seats.length; j++) {
+                        seats[j] = tokens[k++];
+                    }
+                    bys.addAShow(new Show(check[1], tokens[1], seats));
+                break;
+                case "book":
+                    k = 2 + 2;
+                    seats = new String[tokens.length - 2 - 2];
+                    for (int j = 0; j < seats.length; j++) {
+                        seats[j] = tokens[k++];
+                    }
+                    bys.bookAShow(check[1], tokens[1],
+                        new Patron(tokens[2], tokens[2 + 1]), seats);
+                break;
+
+                case "get":
+                    Show show = bys.getAShow(check[1], tokens[1]);
+                    if (show != null) {
+                       System.out.println(show);
+                    } else {
+                        System.out.println("No show");
+                    }
+                break;
+
+                case "print":
+                    bys.printTickets(check[1], tokens[1], tokens[2]);
+                break;
+
+                case "showAll":
+                    bys.showAll();
+                break;
+
+                default:
+                break;
+            }
+        }
+    }
+}
+
+
