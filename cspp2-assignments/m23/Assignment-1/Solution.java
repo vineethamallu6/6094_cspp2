4b7b8f7f95a6414e8c9e70783d63e756f1acd1e9
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-09-20 20:15:04 -0700

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m23/Assignment-1/Solution.java
+++ cspp2-assignments/m23/Assignment-1/Solution.java
@@ -88,9 +88,6 @@ class Solution {
 		readFile = s.nextLine();
 		File folder = new File(readFile);
 		File[] listOfFiles = folder.listFiles();
-		for (File f : listOfFiles) {
-			//System.out.println(f);
-		}
 		int length = listOfFiles.length;
 		int[][] matrix = new int[length][length];
 		for (int i = 0; i<length; i++) {
@@ -103,10 +100,6 @@ class Solution {
 			System.out.print("\t" + listOfFiles[i].getName());
 		}
 		System.out.println("\t" + listOfFiles[length-1].getName());
-		for (int i = 0; i < length; i++) {
-			System.out.print(listOfFiles[i].getName() + "\t");
-
-		}
 		for (int i = 0; i<length;i++) {
 			for(int j = 0;j<length;j++) {
 				System.out.print(matrix[i][j]+"\t");
